6525916ea0f2ae84022ec514049c96ebdedf433b
Lewis.Lee <837597708@qq.com>
2025-05-12 15:56:58 +0800

    draw Board

---
+++ build/CMakeFiles/gomoku.dir/src/main.cpp.o.d
@@ -0,0 +1,11 @@
+CMakeFiles/gomoku.dir/src/main.cpp.o: \
+  /Users/kaihaverz/Desktop/大三下/C&C++/C++/C++大作业/gomoku_game/src/main.cpp \
+  /Users/kaihaverz/Desktop/大三下/C&C++/C++/C++大作业/gomoku_game/include/Board.hpp \
+  /opt/homebrew/Cellar/raylib/5.5/include/raylib.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/stdarg.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/__stdarg_header_macro.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/__stdarg___gnuc_va_list.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/__stdarg_va_list.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/__stdarg_va_arg.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/__stdarg___va_copy.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/__stdarg_va_copy.h
